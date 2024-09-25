820b3da36de0d3e3c11f28d397dbc3fb199520a8
mdTarek-web <mdtarek2330749@gmail.com>
2024-09-25 22:34:33 +0600

    Add files via upload

---
+++ project/Quiz App/question.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb:1,
+        question:'What Does HTML Stand For?'
+        answer:'Hyper Text Markup Language'
+        options:[
+             "Hyper Text Multiple Language",
+             "Hyper Text Markup Language",
+             "Hyper Text Multiple Language",
+             "Hyper Tool Multi Language";
+        ]
+    },
+    {
+        numb:2
+        question:"What Does CSS Stand for?f"
+        answer:"Cascading Style Sheet",
+        options:[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+        
+    },
+    {
+        numb:3,
+        question:"What Does PHP Stand for?",
+        answer:"Hypertext Preprocessor",
+        options:[
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+           " Hypertext preprogramming",
+          " Hometext preprocessor"
+        ]
+        
+    },
+    {
+        numb:4,
+        question:"What does SQL stand for?",
+        answer:"Strutured query Language",
+        options:[
+            "Stylish Question language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+        
+    },
+    {
+        numb:5,
+        question:"What does XML stand for?",
+        answer:"extensible Markup Language",
+        option:[
+            "extensible Markup language",
+            "executable Multiple Language",
+            "exTra Multi-programm Language",
+            "examine Multiple Language"
+        ]
+    }
+
+]
